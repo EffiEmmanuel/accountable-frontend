fb1a0cfd29cea4bf558c3fdef42fd6b75ed2ef48
EffiEmmanuel <effiemmanuel.n#gmail.com>
2023-07-09 22:35:42 +0100

    UPDATE: DONE

--- src/components/ui/Dashboard/Settings/index.jsx
+++ src/components/ui/Dashboard/Settings/index.jsx
@@ -28,7 +28,16 @@ export default function Settings() {
           </div>
 
           <div className="border-[2px] border-[#323232] p-10 w-full border-dashed min-h-[80vh] border-spacing-[10rem] text-center flex flex-col">
-            <div className="flex items-center justify-between w-full">
+            <p className="font-semibold text-left border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              Charts of Account
+            </p>
+            <p className="text-left border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              Company Information
+            </p>
+            <p className="text-left border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              Email
+            </p>
+            <div className="flex items-center justify-between w-full border-b-[0.5px] pb-3 border-b-[#323232]">
               <p>Notification</p>
               <div>
                 <Switch
@@ -37,6 +46,92 @@ export default function Settings() {
                 />
               </div>
             </div>
+
+            <small className="text-gray-500 font-semibold text-left mt-4">
+              Security
+            </small>
+
+            <div className="flex items-center justify-between w-full border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              <p>Fingerprint</p>
+              <div>
+                <Switch
+                  onChange={(value) => setFingerPrint(value)}
+                  checked={fingerprint}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between w-full border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              <p>Screenlock</p>
+              <div>
+                <Switch
+                  onChange={(value) => setScreenLock(value)}
+                  checked={screenLock}
+                />
+              </div>
+            </div>
+
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Import Data
+            </p>
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Export Data
+            </p>
+
+            <div className="flex items-center justify-between w-full border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              <p>Sync</p>
+              <div>
+                <Switch onChange={(value) => setSync(value)} checked={sync} />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between w-full border-b-[0.5px] pb-3 border-b-[#323232] my-4">
+              <p>Last sync</p>
+              <p className="text-gray-500">14:00 | 19 May 2023</p>
+            </div>
+
+            {/* FINANCIAL SETTINGS */}
+
+            <small className="text-gray-500 font-semibold text-left mt-4">
+              Financial Settings
+            </small>
+
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Invoice Settings
+            </p>
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Tax Settings
+            </p>
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Manage Users
+            </p>
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Currencies
+            </p>
+            <p
+              onClick={() => {}}
+              className="font-semibold text-left mt-4 border-b-[0.5px] pb-3 border-b-[#323232] my-4"
+            >
+              Billing / Subscriptions
+            </p>
           </div>
         </div>
       </section>
